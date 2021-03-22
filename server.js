0e48c7d5a3a98d11580c29563b4282d2c918734d
Jostein Tollefsrud <mail@josteintollefsrud.no>
2021-03-22 23:16:10 +0100

    recreated

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const expressLayouts = require('express-ejs-layouts');
 const methodOverride = require('method-override');
 
 const indexRouter = require('./routes/index');
-const authorRouter = require('./routes/authors');
-const bookRouter = require('./routes/books');
+const userRouter = require('./routes/users');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -30,7 +29,6 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
-app.use('/authors', authorRouter);
-app.use('/books', bookRouter);
+app.use('/users', userRouter);
 
 app.listen(process.env.PORT || 3000);
