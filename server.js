63a88ed572bd52ac1b268c7b30d9a8f38cb1ef41
Jostein Tollefsrud <mail@josteintollefsrud.no>
2021-03-22 16:05:28 +0100

    initial author routes

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
+app.use(express.json());
+app.use(express.urlencoded({ limit: '10mb', extended: false }));
 app.use(expressLayouts);
 app.use(express.static('public'));
 
@@ -24,5 +27,6 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
